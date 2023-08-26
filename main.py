8c41f49c86a097328307d1fa8af1d017b28aec82
ace9665 <76694627+ace9665@users.noreply.github.com>
2023-08-26 02:00:24 -0400

    Player sprite instance//adding into a group and updating using pygame.sprite

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 #Importing_Modules
 import pygame
 from sys import exit
+from Player import Player
+from assets import Enemy
 
 #Initialising_Pygame
 pygame.init()
 clock=pygame.time.Clock()
 
+
+
 #Variables
-surf_ground=pygame.Surface([1368,20])
+surf_ground=pygame.Surface([1368,22])
 screen=pygame.display.set_mode((1368,700))
-screen.fill("#9B9B9B")
+
+player=pygame.sprite.GroupSingle()
+player.add(Player())
+enemy=pygame.sprite.GroupSingle()
+enemy.add(Enemy())
 #Game_Loop
 while True:
 
@@ -17,7 +25,16 @@ while True:
         if event.type==pygame.QUIT:
             exit()
     surf_ground.fill("#4A4A4A")
-    screen.blit(surf_ground,(0,550))
+    screen.fill("#9B9B9B")
+    screen.blit(surf_ground, (0, 550))
+    player.draw(screen)
+    enemy.draw(screen)
+    player.update()
 
+
+
+
+    #Updating_screen_after_every_frame
     pygame.display.update()
+    #Fps_Count
     clock.tick(60)
