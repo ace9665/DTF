25081aa325bcaf9381fceca93a4340775f5ccf88
ace9665 <76694627+ace9665@users.noreply.github.com>
2023-08-26 02:00:55 -0400

    just created a random spawning enemy. Work in progress...

--- assets.py
+++ assets.py
@@ -1,4 +1,11 @@
 #Importing_Variables
 import pygame
+from random import randint
 
-pygame.surface
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image=pygame.image.load("assets/enemy/enemy.png")
+        self.image=pygame.transform.rotozoom(self.image,0,10)
+        self.rect=self.image.get_rect(center=(randint(0,1368),475))
+
